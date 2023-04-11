20e5f00e97e89e5677d71be6e5be192ab4dd4b47
The-Rob-Sellers <129602395+The-Rob-Sellers@users.noreply.github.com>
2023-04-11 10:51:53 -0600

    Done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ageToVote = 18
+const ageToVote = 100
 
 const user = {
   name: 'Sherlock Holmes',
@@ -16,6 +16,36 @@ const cases = [
   { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 }
 ]
 
-// YOUR JS CODE
+let currentYear = new Date().getFullYear();
+let jsReleaseYear = 1995;
+let currentAge = currentYear - jsReleaseYear;
 
+console.log(`The current age of JavaScript is ${currentAge} years.`);
+if (currentAge >= ageToVote) {
+  console.log("JavaScript is old enough to vote");
+} else {
+  console.log("JavaScript is not old enough to vote");
+}
 
+console.log('starting')
+for (let i = 0; i <= 100; i += 10) {
+  console.log(i);
+  if (i === 50) {
+    console.log("Halfway")
+  }
+}
+console.log('Finished')
+
+console.log(`${user.name} lives at ${user.address.street}, ${user.address.city}`)
+//
+for (let i = 0; i < cases.length; i++) {
+  const currentCase = cases[i];
+  console.log(`${currentCase.title} ${currentCase.releaseYear}`);
+}
+
+for (let i = 0; i < cases.length; i++) {
+  const currentCase = cases[i];
+  if (currentCase.releaseYear === 1903) {
+    console.log(`${currentCase.title}`);
+  }
+}
